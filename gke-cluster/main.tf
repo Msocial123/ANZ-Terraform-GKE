9dea88330dfd8f9759813e514f0657d43acb489c
Ajnanz <159983737+Ajnanz@users.noreply.github.com>
2025-06-27 15:17:00 +0530

    Update main.tf

--- gke-cluster/main.tf
+++ gke-cluster/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "gcs" {
-    bucket = "amarjit-test-devops"         # ✅ Your existing bucket
+    bucket = "devajnbucket"         # ✅ Your existing bucket
     prefix = "gke-cluster/terraform-state" # ✅ Folder path inside the bucket
   }
 
